dfd111a54c4db927acf9a5ccfa99c237edbba45c
krld <krldboyan@gmail.com>
2013-05-07 16:02:40 +0400

    add libs

---
+++ Life/src/com/krld/Life/Game.java
@@ -0,0 +1,42 @@
+package com.krld.Life;
+
+import com.badlogic.gdx.ApplicationListener;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: krld
+ * Date: 07.05.13
+ * Time: 16:00
+ * To change this template use File | Settings | File Templates.
+ */
+public class Game implements ApplicationListener {
+    @Override
+    public void create() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void resize(int i, int i2) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void render() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void pause() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void resume() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void dispose() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
--- Life/src/com/krld/Life/StartActivity.java
+++ Life/src/com/krld/Life/StartActivity.java
@@ -2,14 +2,16 @@ package com.krld.Life;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.badlogic.gdx.backends.android.AndroidApplication;
 
-public class StartActivity extends Activity {
+public class StartActivity extends AndroidApplication {
     /**
      * Called when the activity is first created.
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initialize(new Game(), false);
         setContentView(R.layout.main);
     }
 }

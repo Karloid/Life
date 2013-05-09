29b357268716d1e8ca0301f5712867d839dfccc5
krld <krldboyan@gmail.com>
2013-05-09 11:59:25 +0400

    create custom icon

--- Life/src/com/krld/Life/MyLifeField.java
+++ Life/src/com/krld/Life/MyLifeField.java
@@ -25,6 +25,7 @@ public class MyLifeField extends Actor {
     public static final int DIMENSION_X = WIDTH_GAME_WINDOW / SIZE;
     public static final int DIMENSION_Y = HEIGHT_GAME_WINDOW / SIZE;
     public static final int SUB_BUTTON_X = SIZE * DIMENSION_X + 100;
+    private static final int SUB_BUTTON_2_X = SUB_BUTTON_X + 50;
     public static final int SEPARATE_LINE_X = SIZE * DIMENSION_X;
     private static final String DEBUG_TAG = "LIFE";
     public static final int HEIGHT_WINDOW = 800;
@@ -85,7 +86,7 @@ public class MyLifeField extends Actor {
                 }
             } else if (y < HEIGHT_ONE_MENU_BUTTON * 2) {
                 if (x > SUB_BUTTON_X) {
-                    setCreateType(9);
+                    setCreateType(8);
                 } else {
                     setCreateType(1);
                 }
@@ -102,7 +103,9 @@ public class MyLifeField extends Actor {
                     setCreateType(3);
                 }
             } else if (y < HEIGHT_ONE_MENU_BUTTON * 5) {
-                if (x > SUB_BUTTON_X) {
+                if (x > SUB_BUTTON_2_X) {
+                    setCreateType(9);
+                } else if (x > SUB_BUTTON_X) {
                     setCreateType(5);
                 } else {
                     setCreateType(4);
@@ -169,37 +172,91 @@ public class MyLifeField extends Actor {
                 field[((int) (x / SIZE)) + 1][((int) (y / SIZE)) + 2] = 1;
             } else if (createType == 7) {       //
                 field[((int) (x / SIZE))][((int) (y / SIZE))] = 1;
-                field[((int) (x / SIZE)) +1][((int) (y / SIZE))] = 1;
-                field[((int) (x / SIZE)) +2][((int) (y / SIZE))] = 1;
-                field[((int) (x / SIZE)) +3][((int) (y / SIZE))] = 1;
-                field[((int) (x / SIZE)) +4][((int) (y / SIZE))] = 1;
-                field[((int) (x / SIZE)) +5][((int) (y / SIZE))] = 1;
-                field[((int) (x / SIZE)) +6][((int) (y / SIZE))] = 1;
-                field[((int) (x / SIZE)) +7][((int) (y / SIZE))] = 1;
+                field[((int) (x / SIZE)) + 1][((int) (y / SIZE))] = 1;
+                field[((int) (x / SIZE)) + 2][((int) (y / SIZE))] = 1;
+                field[((int) (x / SIZE)) + 3][((int) (y / SIZE))] = 1;
+                field[((int) (x / SIZE)) + 4][((int) (y / SIZE))] = 1;
+                field[((int) (x / SIZE)) + 5][((int) (y / SIZE))] = 1;
+                field[((int) (x / SIZE)) + 6][((int) (y / SIZE))] = 1;
+                field[((int) (x / SIZE)) + 7][((int) (y / SIZE))] = 1;
 
-                field[((int) (x / SIZE)) +9][((int) (y / SIZE))] = 1;
-                field[((int) (x / SIZE)) +10][((int) (y / SIZE))] = 1;
-                field[((int) (x / SIZE)) +11][((int) (y / SIZE))] = 1;
-                field[((int) (x / SIZE)) +12][((int) (y / SIZE))] = 1;
-                field[((int) (x / SIZE)) +13][((int) (y / SIZE))] = 1;
+                field[((int) (x / SIZE)) + 9][((int) (y / SIZE))] = 1;
+                field[((int) (x / SIZE)) + 10][((int) (y / SIZE))] = 1;
+                field[((int) (x / SIZE)) + 11][((int) (y / SIZE))] = 1;
+                field[((int) (x / SIZE)) + 12][((int) (y / SIZE))] = 1;
+                field[((int) (x / SIZE)) + 13][((int) (y / SIZE))] = 1;
 
-                field[((int) (x / SIZE)) +17][((int) (y / SIZE))] = 1;
-                field[((int) (x / SIZE)) +18][((int) (y / SIZE))] = 1;
-                field[((int) (x / SIZE)) +19][((int) (y / SIZE))] = 1;
+                field[((int) (x / SIZE)) + 17][((int) (y / SIZE))] = 1;
+                field[((int) (x / SIZE)) + 18][((int) (y / SIZE))] = 1;
+                field[((int) (x / SIZE)) + 19][((int) (y / SIZE))] = 1;
 
-                field[((int) (x / SIZE)) +26][((int) (y / SIZE))] = 1;
-                field[((int) (x / SIZE)) +27][((int) (y / SIZE))] = 1;
-                field[((int) (x / SIZE)) +28][((int) (y / SIZE))] = 1;
-                field[((int) (x / SIZE)) +29][((int) (y / SIZE))] = 1;
-                field[((int) (x / SIZE)) +30][((int) (y / SIZE))] = 1;
-                field[((int) (x / SIZE)) +31][((int) (y / SIZE))] = 1;
-                field[((int) (x / SIZE)) +32][((int) (y / SIZE))] = 1;
+                field[((int) (x / SIZE)) + 26][((int) (y / SIZE))] = 1;
+                field[((int) (x / SIZE)) + 27][((int) (y / SIZE))] = 1;
+                field[((int) (x / SIZE)) + 28][((int) (y / SIZE))] = 1;
+                field[((int) (x / SIZE)) + 29][((int) (y / SIZE))] = 1;
+                field[((int) (x / SIZE)) + 30][((int) (y / SIZE))] = 1;
+                field[((int) (x / SIZE)) + 31][((int) (y / SIZE))] = 1;
+                field[((int) (x / SIZE)) + 32][((int) (y / SIZE))] = 1;
 
-                field[((int) (x / SIZE)) +34][((int) (y / SIZE))] = 1;
-                field[((int) (x / SIZE)) +35][((int) (y / SIZE))] = 1;
-                field[((int) (x / SIZE)) +36][((int) (y / SIZE))] = 1;
-                field[((int) (x / SIZE)) +37][((int) (y / SIZE))] = 1;
-                field[((int) (x / SIZE)) +38][((int) (y / SIZE))] = 1;
+                field[((int) (x / SIZE)) + 34][((int) (y / SIZE))] = 1;
+                field[((int) (x / SIZE)) + 35][((int) (y / SIZE))] = 1;
+                field[((int) (x / SIZE)) + 36][((int) (y / SIZE))] = 1;
+                field[((int) (x / SIZE)) + 37][((int) (y / SIZE))] = 1;
+                field[((int) (x / SIZE)) + 38][((int) (y / SIZE))] = 1;
+            } else if (createType == 8) {       //  gun
+                field[((int) (x / SIZE))][((int) (y / SIZE))] = 1;
+                field[((int) (x / SIZE)) + 1][((int) (y / SIZE))] = 1;
+                field[((int) (x / SIZE)) + 1][((int) (y / SIZE)) + 1] = 1;
+                field[((int) (x / SIZE))][((int) (y / SIZE)) + 1] = 1;
+
+                field[((int) (x / SIZE)) + 10][((int) (y / SIZE))] = 1;
+                field[((int) (x / SIZE)) + 10][((int) (y / SIZE)) + 1] = 1;
+                field[((int) (x / SIZE)) + 10][((int) (y / SIZE)) - 1] = 1;
+                field[((int) (x / SIZE)) + 11][((int) (y / SIZE)) - 2] = 1;
+                field[((int) (x / SIZE)) + 11][((int) (y / SIZE)) + 2] = 1;
+
+                field[((int) (x / SIZE)) + 12][((int) (y / SIZE)) - 3] = 1;
+                field[((int) (x / SIZE)) + 12][((int) (y / SIZE)) + 3] = 1;
+
+                field[((int) (x / SIZE)) + 13][((int) (y / SIZE)) - 3] = 1;
+                field[((int) (x / SIZE)) + 13][((int) (y / SIZE)) + 3] = 1;
+
+                field[((int) (x / SIZE)) + 14][((int) (y / SIZE))] = 1;
+
+                field[((int) (x / SIZE)) + 15][((int) (y / SIZE)) - 2] = 1;
+                field[((int) (x / SIZE)) + 15][((int) (y / SIZE)) + 2] = 1;
+
+                field[((int) (x / SIZE)) + 16][((int) (y / SIZE)) - 1] = 1;
+                field[((int) (x / SIZE)) + 16][((int) (y / SIZE)) + 1] = 1;
+                field[((int) (x / SIZE)) + 16][((int) (y / SIZE))] = 1;
+                field[((int) (x / SIZE)) + 17][((int) (y / SIZE))] = 1;
+
+                field[((int) (x / SIZE)) + 20][((int) (y / SIZE)) + 1] = 1;
+                field[((int) (x / SIZE)) + 20][((int) (y / SIZE)) + 2] = 1;
+                field[((int) (x / SIZE)) + 20][((int) (y / SIZE)) + 3] = 1;
+
+                field[((int) (x / SIZE)) + 21][((int) (y / SIZE)) + 1] = 1;
+                field[((int) (x / SIZE)) + 21][((int) (y / SIZE)) + 2] = 1;
+                field[((int) (x / SIZE)) + 21][((int) (y / SIZE)) + 3] = 1;
+
+                field[((int) (x / SIZE)) + 22][((int) (y / SIZE)) + 4] = 1;
+                field[((int) (x / SIZE)) + 22][((int) (y / SIZE))] = 1;
+
+                field[((int) (x / SIZE)) + 24][((int) (y / SIZE))] = 1;
+                field[((int) (x / SIZE)) + 24][((int) (y / SIZE)) - 1] = 1;
+
+                field[((int) (x / SIZE)) + 24][((int) (y / SIZE)) + 4] = 1;
+                field[((int) (x / SIZE)) + 24][((int) (y / SIZE)) + 5] = 1;
+
+                field[((int) (x / SIZE)) + 34][((int) (y / SIZE)) + 2] = 1;
+                field[((int) (x / SIZE)) + 34][((int) (y / SIZE)) + 3] = 1;
+                field[((int) (x / SIZE)) + 35][((int) (y / SIZE)) + 2] = 1;
+                field[((int) (x / SIZE)) + 35][((int) (y / SIZE)) + 3] = 1;
+
+            }   else if (createType == 9) {       //  pulsar
+                field[((int) (x / SIZE))][((int) (y / SIZE))] = 1;
+
+
             }
         } catch (ArrayIndexOutOfBoundsException e) {
             Log.e(DEBUG_TAG, "HIT " + x + " " + y + " xy " + ((int) (x / SIZE)) + " " + ((int) (y / SIZE)));
@@ -255,6 +312,7 @@ public class MyLifeField extends Actor {
         renderer.filledRect(SEPARATE_LINE_X, 0, WIDTH_SEPARATE_LINE, HEIGHT_WINDOW);
         renderer.setColor(Color.LIGHT_GRAY);
         renderer.filledRect(SUB_BUTTON_X, 0, WIDTH_SEPARATE_LINE, HEIGHT_WINDOW);
+        renderer.filledRect(SUB_BUTTON_2_X, 0, WIDTH_SEPARATE_LINE, HEIGHT_WINDOW);
         renderer.filledRect(SEPARATE_LINE_X, HEIGHT_ONE_MENU_BUTTON, WIDTH_BUTTON_SEPARATE_LINE, HEIGHT_SEPARATE_BUTTON_LINE);
         renderer.filledRect(SEPARATE_LINE_X, HEIGHT_ONE_MENU_BUTTON * 2, WIDTH_BUTTON_SEPARATE_LINE, HEIGHT_SEPARATE_BUTTON_LINE);
         renderer.filledRect(SEPARATE_LINE_X, HEIGHT_ONE_MENU_BUTTON * 3, WIDTH_BUTTON_SEPARATE_LINE, HEIGHT_SEPARATE_BUTTON_LINE);
